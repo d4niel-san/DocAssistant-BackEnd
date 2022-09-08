94d366137382b42047d84f79eac62ccadef9ec54
d4niel-san <91570360+d4niel-san@users.noreply.github.com>
2022-09-08 12:40:58 -0300

    order by desc

--- src/database/queries.js
+++ src/database/queries.js
@@ -7,5 +7,5 @@ export const queries = {
   insertConsulta:
     "INSERT INTO consultas (patientId, date, payed, register) VALUES (15, select convert(varchar, current_timestamp, 113), 0, 'Esto es una pruebaaaa')",
   getConsultaById: "SELECT * FROM Consultas WHERE patientId=",
-  orderByDate: " ORDER BY date;",
+  orderByDate: " ORDER BY date DESC;",
 };
