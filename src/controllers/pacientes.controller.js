9e16080879cc18993c743dc16024110277a0c3a0
d4niel-san <91570360+d4niel-san@users.noreply.github.com>
2022-09-15 11:55:08 -0300

    code cleaning

--- src/controllers/pacientes.controller.js
+++ src/controllers/pacientes.controller.js
@@ -20,6 +20,7 @@ export const getPaciente = async (req, res) => {
   paciente.DNI = formatDNI(paciente.DNI);
   paciente.Cell = formatCell(paciente.Cell);
   paciente.Id = formatId(paciente.Id);
+  console.log("Paciente a devolver: ", paciente);
   res.send(paciente);
 };
 
